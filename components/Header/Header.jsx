2fa473c17b8dba531b940630882745a8ba17997e
Saman <samanahrari@yahoo.com>
2022-04-13 23:00:22 +0430

    added header component and modified some classes in grid container

---
+++ components/Header/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <header className="row-start-1 flex row-end-1 col-start-2 col-end-13 h-[60px] p-[10px]"></header>
+  );
+};
+
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
+import Header from "../components/Header/Header";
 import NavBar from "../components/Navigation/NavBar";
 import "../index.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <div className="w-[100vw] h-[100vh] auto-rows-max bg-black grid grid-cols-12 grid-rows-[repeat(12,minmax(0,1fr))]">
+      <div className="w-[100vw] h-[100vh] bg-black grid grid-cols-[repeat(12,1fr)] grid-rows-[repeat(12,1fr)]">
+        <Header />
         <NavBar />
         <Component {...pageProps} />
       </div>

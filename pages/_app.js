309534de16e1569465b248b8362bdec733258d9c
Saman <samanahrari@yahoo.com>
2022-04-13 22:10:50 +0430

    added grid to the components container

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import "../index.css";
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <div className="w-[100vw] h-[100vh] bg-black">
+      <div className="w-[100vw] h-[100vh] auto-rows-max bg-black grid grid-cols-12 grid-rows-[repeat(12,minmax(0,1fr))]">
         <NavBar />
       </div>
       <Component {...pageProps} />

0377ab0b57f3283bfcd2cead29eebbd0026cc0cd
Saman <samanahrari@yahoo.com>
2022-04-30 13:52:00 +0430

    added title to tracks list

--- components/MusicList/MusicListContainer.jsx
+++ components/MusicList/MusicListContainer.jsx
@@ -1,7 +1,10 @@
 const MusicListContainer = (props) => {
   return (
-    <div className="w-full md:max-w-[650px] md:min-w-fit mb-[20px] max-h-[550px] min-h-fit gap-y-[15px] rounded-[20px] overflow-y-auto bg-[#0d0d0d] p-[20px] border-2 border-[#1e1e1e] border-solid flex flex-col">
-      {props.children}
+    <div className="w-full md:max-w-[650px] md:min-w-fit mb-[20px] max-h-[550px] min-h-fit">
+      <h2 className="text-[18px] text-white mb-[10px]">Tracks of the Week</h2>
+      <div className="w-full h-full gap-y-[15px] rounded-[20px] overflow-y-auto bg-[#0d0d0d] p-[20px] border-2 border-[#1e1e1e] border-solid flex flex-col">
+        {props.children}
+      </div>
     </div>
   );
 };

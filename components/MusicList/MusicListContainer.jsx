73d2de84251f53df055b1a411f37d4c573e03143
Saman <samanahrari@yahoo.com>
2022-04-30 13:20:37 +0430

    made music list and genres responsive

--- components/MusicList/MusicListContainer.jsx
+++ components/MusicList/MusicListContainer.jsx
@@ -1,6 +1,6 @@
 const MusicListContainer = (props) => {
   return (
-    <div className="max-h-[550px] gap-y-[15px] rounded-[20px] overflow-y-auto bg-[#0d0d0d] p-[20px] border-2 border-[#1e1e1e] border-solid flex min-w-[680px] max-w-[680px] flex-col">
+    <div className="w-full mb-[20px] md:max-h-[550px] gap-y-[15px] rounded-[20px] overflow-y-auto bg-[#0d0d0d] p-[20px] border-2 border-[#1e1e1e] border-solid flex flex-col">
       {props.children}
     </div>
   );

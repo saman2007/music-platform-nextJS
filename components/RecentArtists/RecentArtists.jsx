ef7b81bbfc29b8f1c229e33454f78b8be7141df7
Saman <samanahrari@yahoo.com>
2022-05-01 20:20:49 +0430

    added title to recent artists

--- components/RecentArtists/RecentArtists.jsx
+++ components/RecentArtists/RecentArtists.jsx
@@ -4,16 +4,19 @@ const RecentArtists = (props) => {
   const { artists } = props;
 
   return (
-    <div className="max-w-[200px] max-h-[200px] min-w-[200px] min-h-[200px] flex flex-wrap gap-[5px]">
-      {artists.map((data) => (
-        <Artist
-          background={data.background}
-          color={data.color}
-          radius={data.radius}
-          page={data.page}
-          key={data.id}
-        />
-      ))}
+    <div className="max-w-[200px] max-h-[200px] min-w-[200px] min-h-[200px] mb-[20px]">
+      <h2 className="text-[18px] text-white mb-[10px]">Recent Artists</h2>
+      <div className=" flex flex-wrap gap-[5px]">
+        {artists.map((data) => (
+          <Artist
+            background={data.background}
+            color={data.color}
+            radius={data.radius}
+            page={data.page}
+            key={data.id}
+          />
+        ))}
+      </div>
     </div>
   );
 };

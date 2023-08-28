3946b1d4ad6074db47b34a3e68218d9f2d9eb995
sami_dev <samanahrari@yahoo.com>
2023-08-28 13:25:22 +0330

    FEATURE: added google analytics for test

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,11 @@ import { createClient } from "@supabase/supabase-js";
 import "../index.css";
 import Notification from "../components/notification/Notification";
 import ContextProvider from "../store/context";
-import Router from "next/router";
+import Router, { useRouter } from "next/router";
 import nprogress from "nprogress";
+import * as gtag from "../utils/gtag";
+import { useEffect } from "react";
+import Script from "next/script";
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_API_URL,
@@ -20,8 +23,37 @@ Router.events.on("routeChangeError", nprogress.done);
 Router.events.on("routeChangeComplete", nprogress.done);
 
 function MyApp({ Component, pageProps, genres }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ContextProvider>
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        onError={(e) => {
+          console.log("ana", e);
+        }}
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Provider store={store}>
         <Notification />
         <div

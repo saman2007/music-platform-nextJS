ec7820ddcfdf533c3018f2dcff061eec3fb022de
sami_dev <samanahrari@yahoo.com>
2023-09-01 13:09:38 +0330

    MODIFIED: modified the way of loading GTM and GA

--- pages/_app.js
+++ pages/_app.js
@@ -7,11 +7,10 @@ import { createClient } from "@supabase/supabase-js";
 import "../index.css";
 import Notification from "../components/notification/Notification";
 import ContextProvider from "../store/context";
-import Router from "next/router";
+import Router, { useRouter } from "next/router";
 import nprogress from "nprogress";
-import { GoogleAnalytics } from "nextjs-google-analytics";
-import GoogleTagManager from "react-gtm-module";
-import { useEffect } from "react";
+import Script from "next/script";
+import { useEffect, useRef } from "react";
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_API_URL,
@@ -23,27 +22,70 @@ Router.events.on("routeChangeError", nprogress.done);
 Router.events.on("routeChangeComplete", nprogress.done);
 
 function MyApp({ Component, pageProps, genres }) {
-  useEffect(() => {
-    GoogleTagManager.initialize({
-      gtmId: process.env.NEXT_PUBLIC_GTM_MEASUREMENT_ID,
+  const router = useRouter();
+  const isFirstRender = useRef(false);
+
+  const pageview = () => {
+    if (!isFirstRender.current) {
+      isFirstRender.current = true;
+      return;
+    }
+
+    window.dataLayer.push({
+      event: "gtm.load",
     });
-  }, []);
+  };
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", pageview);
+
+    return () => {
+      router.events.off("routeChangeComplete", pageview);
+    };
+  }, [router.events]);
 
   return (
-    <ContextProvider>
-      <GoogleAnalytics trackPageViews={{ ignoreHashChange: true }} />
-      <Provider store={store}>
-        <Notification />
-        <div
-          className={`relative w-full h-full overflow-hidden select-none bg-[#f8f9fa] dark:bg-black grid grid-cols-[70px,repeat(11,1fr)] grid-rows-[repeat(11,1fr),65px]`}
-        >
-          <Header genres={genres} />
-          <NavBar />
-          <MusicActionsBar />
-          <Component {...pageProps} />
-        </div>
-      </Provider>
-    </ContextProvider>
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTM_MEASUREMENT_ID}');
+          `,
+        }}
+      />
+      <ContextProvider>
+        <Provider store={store}>
+          <Notification />
+          <div
+            className={`relative page-container w-full h-full overflow-hidden select-none bg-[#f8f9fa] dark:bg-black grid grid-cols-[70px,repeat(11,1fr)] grid-rows-[repeat(11,1fr),65px]`}
+          >
+            <Header genres={genres} />
+            <NavBar />
+            <MusicActionsBar />
+            <Component {...pageProps} />
+          </div>
+        </Provider>
+      </ContextProvider>
+    </>
   );
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,14 @@ class MyDocument extends Document {
       <Html className="w-full h-full">
         <Head />
         <body className="w-full h-full scroll-smooth dark:bg-[#343434] bg-[#171717]">
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_MEASUREMENT_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
